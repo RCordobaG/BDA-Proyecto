5b24aee5ae28d26dcdedcf092a45bf4f10291145
Rodrigo Cordoba <rcordobagom@gmail.com>
2023-06-12 08:15:17 -0600

    Final modal fixes to headers and content

--- resources/views/GuilleHealing.blade.php
+++ resources/views/GuilleHealing.blade.php
@@ -45,7 +45,7 @@
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                            <div class="modal-title" id="staticBackdropLabel"><h2 class = "text-black">{{$getMedico->MATRICULA}}</h2></div>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
