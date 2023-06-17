34cd20bf7a5f0ac829a18ab284b2ae096bf1b36b
Rodrigo Cordoba <rcordobagom@gmail.com>
2023-06-16 23:16:36 -0600

    Fotos en CRUD (solo create)

--- resources/views/estudios.blade.php
+++ resources/views/estudios.blade.php
@@ -6,7 +6,9 @@
             <h2 class="text-white">Estudios</h2>
         </div>
         <div>
-            <a href="" class="btn btn-primary">Crear tarea</a>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#crearModal">
+                Crear tarea
+                </button>
         </div>
     </div>
 
@@ -21,23 +23,49 @@
                 <th>Audio</th>
                 <th>Acciones</th>
             </tr>
-            @foreach($getMedico as $getMedico)
+            @foreach($estudios as $estudios)
 
             <tr>
-                <td class="fw-bold">{{$getMedico->NumeroEstudio}}</td>
-                <td class="fw-bold">{{$getMedico->NombreEstudio}}</td>
-                <td class="fw-bold">{{$getMedico->IDPaciente}}</td>
+                <td class="fw-bold">{{$estudios->NumeroEstudio}}</td>
+                <td class="fw-bold">{{$estudios->NombreEstudio}}</td>
+                <td class="fw-bold">{{$estudios->IDPaciente}}</td>
                 <td>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-                        <img src="data:image/png;base64,{{chunk_split(base64_encode($getMedico->Imagen))}}"/>
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop{{$estudios->NumeroEstudio}}">
+                        <img src="{{(base64_encode($estudios->Archivo))}}">
                     </button>
                 </td>
-                <td>{{$getMedico->Interpretacion}}</td>
-                <td><audio controls src="data:audio/mp3;base64,{{chunk_split(base64_encode($getMedico->Audio))}}"></audio></td>
-                <td></td>
+                <td>{{$estudios->Interpretacion}}</td>
+                <td><audio controls src="data:audio/mp3;base64,{{chunk_split(base64_encode($estudios->Audio))}}"></audio></td>
+                <td><video controls src="data:audio/mp3;base64,{{chunk_split(base64_encode($estudios->Audio))}}"></video></td>
+                <td>    
+                <form action="{{ route('estudios.destroy',$estudios->NumeroEstudio) }}" method="Post">
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editarModal{{$estudios->NumeroEstudio}}">Editar</button>
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
 
             </tr>
-            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal fade" id="staticBackdrop{{$estudios->NumeroEstudio}}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog"></div>
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <img src="data:image/png;base64,{{chunk_split(base64_encode($estudios->Archivo))}}">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Understood</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="modal fade" id="editarModal{{$estudios->NumeroEstudio}}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                     <div class="modal-header">
@@ -45,10 +73,65 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <video width="200" height="200" controls>
-                        
-                        </video>
+                    <form action="{{ route('estudios.update',$estudios->NumeroEstudio) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="row">
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>ID:</strong>
+                        <input type="text" name="NumeroEstudio" value="{{ $estudios->NumeroEstudio }}" class="form-control"
+                            placeholder="ID">
+                        @error('NumeroEstudio')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
                     </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Nombre:</strong>
+                        <input type="text" name="NombreEstudio" class="form-control" placeholder="Nombre"
+                            value="{{ $estudios->NombreEstudio }}">
+                        @error('NombreEstudio')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Direccion:</strong>
+                        <input type="text" name="IDPaciente" value="{{ $estudios->IDPaciente }}" class="form-control"
+                            placeholder="Direccion">
+                        @error('Direccion')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Foto:</strong>
+                            <input type="file" name="FotoInputUpdate" id = "FotoInputUpdate" src = "" class="form-control" placeholder="Archivo">
+                            @error('FotoInput')
+                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                            @enderror
+                        </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Foto:</strong>
+                            <input type="text" value = "{{$estudios->Archivo}}" name="FotoUpdate" id = "FotoUpdate" class="moleConPollo" placeholder="Archivo">
+                            @error('Foto')
+                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                            @enderror
+                        </div>
+                </div>
+                <button type="submit" class="btn btn-primary ml-3">Submit</button>
+            </div>
+        </form>
+                    </div>
+
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                         <button type="button" class="btn btn-primary">Understood</button>
@@ -59,6 +142,123 @@
             @endforeach
         </table>
     </div>
+
+    <div class="modal fade" id="crearModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="{{ route('estudios.store') }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <div class="row">
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Estudio:</strong>
+                                        <input type="text" name="NombreEstudio" class="form-control" placeholder="Tipo de estudio">
+                                        @error('NombreEstudio')
+                                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>ID Paciente:</strong>
+                                        <input type="text" name="IDPaciente" class="form-control" placeholder="Paciente #">
+                                        @error('IDPaciente')
+                                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Archivo:</strong>
+                                        <input type="file" name="FotoInput" id="FotoInput" class="form-control" placeholder="Archivo">
+                                        @error('FotoInput')
+                                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                        <div class="form-group">
+                            <strong>Foto:</strong>
+                            <input type="text" value = "" name="Foto" id = "Foto" class="mole" placeholder="Archivo">
+                            @error('Foto')
+                            <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                            @enderror
+                        </div>
+                </div>
+
+                <div class="box">
+                    <div class="images">
+                    <img class="mole" />
+                    <img class="mole" />
+                    </div>
+                </div>
+
+            <script>
+                const fileInput = document.getElementById('FotoInput');
+
+                const images =  document.querySelectorAll('.mole');
+
+                fileInput.addEventListener('change', (e) =>{
+                    const file = e.target.files[0];
+
+                    let fileReader = new FileReader();
+                    fileReader.readAsDataURL(file);
+                    fileReader.onload = function (){
+                        images[0].setAttribute('value', fileReader.result);
+                        images[1].setAttribute('src', fileReader.result);
+                        console.log(fileReader.result);
+                        // images[0].setAttribute('style', `background-image: url('${fileReader.result}')`);
+                    }
+                    
+                })
+
+            </script>
+            
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Interpretacion: </strong>
+                                        <input type="text" name="Interpretacion" class="form-control" placeholder="Interpretacion">
+                                        @error('Interpretacion')
+                                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Audio:</strong>
+                                        <input type="file" name="Audio" class="form-control" placeholder="Audio">
+                                        @error('Audio')
+                                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="col-xs-12 col-sm-12 col-md-12">
+                                    <div class="form-group">
+                                        <strong>Video:</strong>
+                                        <input type="file" name="Video" class="form-control" placeholder="Video">
+                                        @error('Video')
+                                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <button type="submit" class="btn btn-primary ml-3">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Understood</button>
+                    </div>
+                    </div>
+                </div>
+            </div>
+
 </div>
 @endsection
-
--- resources/views/pacientes.blade.php
+++ resources/views/pacientes.blade.php
@@ -1,35 +1,42 @@
+<meta charset="UTF-8">
 @extends ('layout.base')
 @section ('content')
 <div class="row">
     <div class="col-12">
         <div>
-            <h2 class="text-white">Medicos</h2>
+            <h2 class="text-white">Pacientes</h2>
         </div>
         <div>
-          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#crearModal">
+            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#crearModal">
                 Crear tarea
-                </button>
+            </button>
         </div>
     </div>
 
     <div class="col-12 mt-4">
         <table class="table table-bordered text-white">
             <tr class="text-secondary">
-                <th>Matricula</th>
-                <th>Nombre</th>
-                <th>Especialidad</th>
+                <th>ID</th>
                 <th>Foto</th>
+                <th>Nombre</th>
+                <th>Direccion</th>
+                <th>Telefono</th>
+                <th>email</th>
+                <th>Medico Tratante</th>
                 <th>Acciones</th>
             </tr>
-            @foreach($getMedico as $getMedico)
+            @foreach($pacientes as $pacientes)
             <tr>
-                <td class="fw-bold">{{$getMedico->MATRICULA}}</td>
-                <td class="fw-bold">{{$getMedico->Nombre}}</td>
-                <td class="fw-bold">{{$getMedico->Especialidad}}</td>
-                <td><img src="{{$getMedico->Foto}}"/></td>
+                <td class="fw-bold">{{$pacientes->ID}}</td>
+                <td><img src="{{($pacientes->Foto)}}"/></td>
+                <td class="fw-bold">{{$pacientes->Nombre}}</td>
+                <td class="fw-bold">{{$pacientes->Direccion}}</td>
+                <td class="fw-bold">{{$pacientes->Telefono}}</td>
+                <td class="fw-bold">{{$pacientes->email}}</td>
+                <td class="fw-bold">{{$pacientes->MedicoTratante}}</td>
                 <td>
-                    <form action="{{ route('medicos.destroy',$getMedico->MATRICULA) }}" method="Post">
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editarModal{{$getMedico->MATRICULA}}">
+                    <form action="{{ route('pacientes.destroy',$pacientes->ID) }}" method="Post">
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editarModal{{$pacientes->ID}}">
                         Crear tarea
                     </button>
                         @csrf
@@ -38,7 +45,7 @@
                     </form>
                 </td>
             </tr>
-            <div class="modal fade" id="editarModal{{$getMedico->MATRICULA}}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal fade" id="editarModal{{$pacientes->ID}}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                 <div class="modal-dialog">
                     <div class="modal-content">
                     <div class="modal-header">
@@ -46,14 +53,14 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                    <form action="{{ route('medicos.update',$getMedico->MATRICULA) }}" method="POST" enctype="multipart/form-data">
+                    <form action="{{ route('pacientes.update',$pacientes->ID) }}" method="POST" enctype="multipart/form-data">
             @csrf
             @method('PUT')
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
                         <strong>ID:</strong>
-                        <input type="text" name="MATRICULA" value="{{ $getMedico->MATRICULA }}" class="form-control"
+                        <input type="text" name="ID" value="{{ $pacientes->ID }}" class="form-control"
                             placeholder="ID">
                         @error('ID')
                         <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
@@ -63,8 +70,8 @@
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
                         <strong>Nombre:</strong>
-                        <input type="text" name="Nombre" value="{{$getMedico->Nombre}}" class="form-control" placeholder="Nombre"
-                            value="{{ $getMedico->Nombre }}">
+                        <input type="text" name="Nombre" class="form-control" placeholder="Nombre"
+                            value="{{ $pacientes->Nombre }}">
                         @error('Nombre')
                         <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                         @enderror
@@ -73,19 +80,48 @@
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
                         <strong>Direccion:</strong>
-                        <input type="text" name="Especialidad" value="{{ $getMedico->Especialidad }}" class="form-control"
+                        <input type="text" name="Direccion" value="{{ $pacientes->Direccion }}" class="form-control"
                             placeholder="Direccion">
-                        @error('Especialidad')
+                        @error('Direccion')
                         <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                         @enderror
                     </div>
                 </div>
-                
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Telefono:</strong>
+                        <input type="text" name="Telefono" value="{{ $pacientes->Telefono }}" class="form-control"
+                            placeholder="Telefono">
+                        @error('Telefono')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>email:</strong>
+                        <input type="email" name="email" value="{{ $pacientes->email }}" class="form-control"
+                            placeholder="email">
+                        @error('email')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Archivo:</strong>
+                        <input type="text" name="MedicoTratante" value="{{ $pacientes->MedicoTratante }}" class="form-control"
+                            placeholder="Matricula Medico">
+                        @error('MedicoTratante')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-12">
                         <div class="form-group">
                             <strong>Foto:</strong>
-                            <input type="file" name="FotoInputUpdate" id = "FotoInputUpdate" value = "{{$getMedico->Foto}}" class="form-control" placeholder="Archivo">
+                            <input type="file" name="FotoInputUpdate" id = "FotoInputUpdate" src = "" class="form-control" placeholder="Archivo">
                             @error('FotoInput')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                             @enderror
@@ -95,7 +131,7 @@
                 <div class="col-xs-12 col-sm-12 col-md-12">
                         <div class="form-group">
                             <strong>Foto:</strong>
-                            <input type="text" value = "{{$getMedico->Foto}}" name="FotoUpdate" id = "FotoUpdate" class="moleConPollo" placeholder="Archivo">
+                            <input type="text" value = "{{$pacientes->Foto}}" name="FotoUpdate" id = "FotoUpdate" class="moleConPollo" placeholder="Archivo">
                             @error('Foto')
                             <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                             @enderror
@@ -126,14 +162,14 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                    <form action="{{ route('medicos.store') }}" method="POST" enctype="multipart/form-data">
+                    <form action="{{ route('pacientes.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
-                        <strong>Matricula:</strong>
-                        <input type="text" name="MATRICULA" class="form-control" placeholder="# Matricula">
-                        @error('MATRICULA')
+                        <h3>Matricula:</h3>
+                        <input type="text" name="ID" class="form-control" placeholder="# Matricula">
+                        @error('ID')
                         <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                         @enderror
                     </div>
@@ -149,13 +185,32 @@
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
-                        <strong>Especialidad:</strong>
-                        <input type="text" name="Especialidad" class="form-control" placeholder="Especialidad">
-                        @error('Especialidad')
+                        <strong>Direccion:</strong>
+                        <input type="text" name="Direccion" class="form-control" placeholder="Direccion">
+                        @error('Direccion')
                         <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
                         @enderror
                     </div>
                 </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>Telefono:</strong>
+                        <input type="text" name="Telefono" class="form-control" placeholder="Telefono">
+                        @error('Telefono')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
+                        <strong>email:</strong>
+                        <input type="email" name="email" class="form-control" placeholder="email">
+                        @error('email')
+                        <div class="alert alert-danger mt-1 mb-1">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
+
                 <div class="col-xs-12 col-sm-12 col-md-12">
                         <div class="form-group">
                             <strong>Foto:</strong>
@@ -203,9 +258,13 @@
                 })
 
             </script>
+                
+
                 <button type="submit" class="btn btn-primary ml-3">Submit</button>
             </div>
         </form>
+
+
                     </div>
 
                     <div class="modal-footer">
@@ -215,5 +274,7 @@
                     </div>
                 </div>
             </div>
+
+    
 @endsection
 
